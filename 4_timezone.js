d716f7346887b20ef83115dabed4c1f0329af3dd
Denis Zhurov <den_fobos@mail.ru>
2021-05-18 23:51:30 +0300

    refactored function with promises

--- 4_timezone.js
+++ 4_timezone.js
@@ -3,45 +3,52 @@ const tOutput = document.querySelector('.time');
 const btnGet = document.querySelector('.btnRequest');
 
 function getPosition() {
-   let geo = navigator.geolocation;
-   geo.getCurrentPosition(getTimezone, 
-    e => {console.log(`Error ${e.code}: ${e.message}`);})
-    
-    function getTimezone (place) {
-        // `https://api.ipgeolocation.io/timezone?apiKey=32bcd4a6e4b548968e7afcdb682ac679&lat=latitude&long=longitude`
-        const apiURL = 'https://api.ipgeolocation.io/timezone';
-        const apiKey = '32bcd4a6e4b548968e7afcdb682ac679';
-        let lat = place.coords.latitude;
-        let lon = place.coords.longitude;
-        let lang = 'ru';
-    
-        let url = new URL (apiURL);
-        url.searchParams.set('apiKey', apiKey);
-        if (lang) url.searchParams.set('lang', lang);
-        if (lat && lon) {
-        url.searchParams.set('lat', lat);
-        url.searchParams.set('lon', lon);
-        }
-        fetch(url)
-        .then(response => {return response.json();})
-        .then(data => {
-            tzOutput.textContent = '';
-            tOutput.textContent = '';
-            tzOutput.innerHTML = '<p>Timezone</p>';
-            tOutput.innerHTML = '<p>Time</p>';  
-            
-            let timezone = data.timezone_offset;
-            if (timezone > 0) timezone = '+' + timezone;
-            tzOutput.append(timezone);
-            tOutput.append(data.date_time_txt);
+    return new Promise(function(resolve, reject) {
+        let geo = navigator.geolocation;
+        geo.getCurrentPosition(position => resolve(position), error => reject(
+        console.log(`Error ${error.code}: ${error.message}`) ))
     });
+} 
+
+function getUrl (geoResponse) {
+    // `https://api.ipgeolocation.io/timezone?apiKey=32bcd4a6e4b548968e7afcdb682ac679&lat=latitude&long=longitude`
+    const apiURL = 'https://api.ipgeolocation.io/timezone';
+    const apiKey = '32bcd4a6e4b548968e7afcdb682ac679';
+    let lat = geoResponse.coords.latitude;
+    let lon = geoResponse.coords.longitude;
+    let lang = 'ru';
+
+    let url = new URL (apiURL);
+    url.searchParams.set('apiKey', apiKey);
+    if (lang) url.searchParams.set('lang', lang);
+    if (lat && lon) {
+    url.searchParams.set('lat', lat);
+    url.searchParams.set('lon', lon);
     }
+    return url.href;
+}
     
+function getTimezoneData(url) {
+    return fetch(url)
+    .then( response => response.json() )
+}
+
+function displayData(data) {
+    tzOutput.textContent = '';
+    tOutput.textContent = '';
+    tzOutput.innerHTML = '<p>Timezone</p>';
+    tOutput.innerHTML = '<p>Time</p>';  
+    
+    let timezone = data.timezone_offset;
+    if (timezone > 0) timezone = '+' + timezone;
+    tzOutput.append(timezone);
+    tOutput.append(data.date_time_txt);
 }
 
 
-
 btnGet.addEventListener('click', async () => {
-    getPosition();
-    
+    let pos = await getPosition();
+    let urlRequest = getUrl(pos);
+    let tzdata = await getTimezoneData(urlRequest);
+    displayData(tzdata);
 });
