0a847846e091f4240123c27d50f09b4f25f2d1bd
Denis Zhurov <den_fobos@mail.ru>
2021-05-19 22:13:07 +0300

    finished making base functions

---
+++ 5_websocket_chat.js
@@ -0,0 +1,104 @@
+const chatWindow = document.querySelector('.container')
+const messageOut = document.querySelector('.messages');
+const btnSend = document.querySelector('.btnSend');
+const btnGeo = document.querySelector('.btnGeo');
+const messageInput = document.querySelector('.message');
+let websocket = new WebSocket('wss://echo.websocket.org/');
+
+
+class Message {
+    constructor(type) {
+        this.type = type,
+        this.content = {},
+        this.timestamp = new Date().getTime();
+    }
+}
+
+//print out messages into message area
+function printMessage(msg, direction) {
+    const p = document.createElement('P');
+    p.classList.add(direction);
+    const span = document.createElement('SPAN');
+
+    span.textContent = msg;
+    p.appendChild(span);
+    messageOut.appendChild(p);
+}
+
+function getLocation() {
+    return new Promise(function(resolve, reject){
+        const geo = navigator.geolocation;
+        geo.getCurrentPosition( position => resolve(position), err => reject(console.log(new Error(err))) );
+    });
+}
+
+//send location aquired from browser API
+function sendLocation(location) {
+    let lat = location.coords.latitude;
+    let long = location.coords.longitude;
+    let position = new Message('geolocation');
+    position.content = {
+        latitude: lat,
+        longitude: long,
+        accuracy: location.coords.accuracy,
+    }
+    
+    let json = JSON.stringify(position);
+
+    let a = document.createElement('A');
+    a.href = `https://www.openstreetmap.org/#map=16/${lat}/${long}`;
+    a.textContent = 'Я здесь!';
+    a.target = '_blank';
+    a.style.textDecoration = 'none';
+    a.style.color = '#1684df';
+
+    let span = document.createElement('SPAN');
+    span.appendChild(a)
+
+    let p = document.createElement('P');
+    p.classList.add('myMessage');
+    p.appendChild(span);
+    messageOut.appendChild(p);
+    //how to check if websocket open?
+    websocket.send(json);
+}
+
+//send text messages to remote server
+function sendMessage(msgText) {
+    let message = new Message('text');
+    if(typeof msgText === 'string') {
+        message.content.text = msgText;
+        printMessage(msgText, 'myMessage');
+        websocket.send(JSON.stringify(message));
+    } else console.log('This type of content is not supported yet');
+}
+
+function receiveMessage(msgJSON) {
+    let message = JSON.parse(msgJSON);
+    if (message.type === 'text') {
+        let text = message.content.text;
+        printMessage(text, 'oppMessage');
+    }
+}
+
+websocket.onopen = function () {
+    console.log('websocket open');
+    btnSend.addEventListener('click', () => {
+        let message = messageInput.value;
+        messageInput.value = '';
+        if (message) sendMessage(message);
+    });
+
+    btnGeo.addEventListener('click', async () => {
+        let location = await getLocation();
+        sendLocation(location);
+    });
+    websocket.onmessage = function(msg) {
+        receiveMessage(msg.data);
+    }
+    websocket.onerror = function(event) {
+        console.log('Error: ' + event.data);
+    }
+}
+
+
